b5bcbe44cb76f4eb7cffaa6433068a5ee207c05b
Oorjita Sharma <oorjitasharma3@gmail.com>
2026-06-02 22:27:52 +0530

    Fix environment variable

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ conn.close()
 
 #GROQ
 client = Groq(
-    api_key="my_api_key"
+    api_key=os.getenv("GROQ_API_KEY")
 )
 
 #HOME
@@ -343,4 +343,7 @@ QUESTION:
 
 #APP RUN
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
